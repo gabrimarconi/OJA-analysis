330b5d8d320a4d8ad9f81780d3a13e80490d3952
gabrimarconi <gabriele.marconi@ext.ec.europa.eu>
2021-02-08 15:19:33 +0000

    i have cleaned a bit the code and added some explanation

--- check_GeoVariables.R
+++ check_GeoVariables.R
@@ -1,56 +1,59 @@
 
+
+
+### packages and libraries
+
 install.packages("gdata")
 install.packages("readxl")
-install.packages("openxlsx")
 library(gdata)
 library(wihoja)
 library(readxl)
-library(openxlsx)
 
 
-country <- "IT"
-year <- 2019
-limit <- 1000
-
+### download Eurostat territorial classifications
 
 download.file("https://ec.europa.eu/eurostat/documents/345175/501971/EU-28-LAU-2019-NUTS-2016.xlsx ", destfile="correspondence.xlsx")
-corresp_table <- read_excel("correspondence.xlsx" , sheet = country)
-colnames(corresp_table) <- substr(str_replace_all(colnames(corresp_table) , " " , "_") , 1 , 11)
-dim(corresp_table)
-NUTS_table <- as.data.frame(table(corresp_table$NUTS_3_CODE))
-colnames(NUTS_table) <- c("NUTS_3_CODE", "Freq")
-NUTS_table$NUTS_3_CODE_m <- NUTS_table$NUTS_3_CODE
-dim(NUTS_table)
-View(NUTS_table)
+#download.file("https://ec.europa.eu/eurostat/documents/345175/501971/EU-28_LAU_2017_NUTS_2013.xlsx ", destfile="correspondence.xlsx")
 
-View(geo_query)
-View(NUTS_check_table)
 
+### set-up a function to check whether idprovince codes have an exact NUTS3 code match
 
 NUTSCHECK_country <- function(country , year=2019 , limit=99999) {
+        #import NUTS classification and generate a table ready for a merge (including a merging key which name ends in "_m")
         corresp_table <- read_excel("correspondence.xlsx" , sheet = country)
         colnames(corresp_table) <- substr(str_replace_all(colnames(corresp_table) , " " , "_") , 1 , 11)
         NUTS_table <- as.data.frame(table(corresp_table$NUTS_3_CODE))
         colnames(NUTS_table) <- c("NUTS_3_CODE", "Freq")
         NUTS_table$NUTS_3_CODE_m <- NUTS_table$NUTS_3_CODE
         
+        #import data from OJA dataset and prepare the table for merge
         query_text <- paste0("SELECT idprovince, province, general_id FROM estat_dsl2531b_oja.ft_document_en_v8 WHERE ((year_grab_date=",year,") AND (idcountry='",country,"')) ORDER BY RAND() LIMIT ",limit)
         geo_query <- get_data(query_text)
         geo_query <- geo_query[idprovince!=""]
         geo_query$NUTS_3_CODE_m <- geo_query$idprovince
         
+        #merge the two tables from the NUTS classification and from the OJA dataset
         NUTS_check_table <- merge(geo_query , NUTS_table , all=TRUE)
+        
+        #create a red flag variable indicating if a match for the OJA observation is not found in the NUTS classification, and create a table containing red-flagged observations
         NUTS_check_table$RedFlag <- 0
         NUTS_check_table$RedFlag[NUTS_check_table$NUTS_3_CODE!=NUTS_check_table$idprovince] <- 1
         NUTS_check_table$RedFlag[is.na(NUTS_check_table$NUTS_3_CODE)==TRUE] <- 1
-        
         NUTS_problems_table <- NUTS_check_table[NUTS_check_table$RedFlag == 1 , ]
+        
+        #generate an output list
         output <- list(dim(geo_query) , dim(NUTS_check_table) , sum(NUTS_check_table$RedFlag) ,  NUTS_problems_table)
         return(output)
 }
 
+
+### apply the previous function to all European countries
+
 countrylist <- c("BE","BG","CZ","DK","DE","EE","IE","EL","ES","FR","HR","IT","CY","LV","LT","LU","HU","MT","NL","AT","PL","PT","RO","SI","SK","FI","SE")
 NUTSCHECK_results <- lapply(countrylist , NUTSCHECK_country)
+
+
+### show and export summary output
 
 printfct_NUTSCHECK <- function(i) {
         a <- countrylist[[i]]
@@ -58,15 +61,14 @@ printfct_NUTSCHECK <- function(i) {
         c <- c(a,b)
         return(print(c))
 }
-
 length <- as.matrix(1:length(countrylist))
 summary_NUTSCHECK <- t(as.matrix(apply(length,1,printfct_NUTSCHECK)))
 summary_NUTSCHECK <- as.data.frame(summary_NUTSCHECK)
 colnames(summary_NUTSCHECK) <- c("country" , "red_flags")
-View(summary_NUTSCHECK)
+write.csv(summary_NUTSCHECK, "GeoVariables_NUTSChecks.csv")
 
 
-   
+### repeat for LAUs/cities the procedure that has been set up for NUTS/provinces  
      
 LAUCHECK_country <- function(country, year=2019, limit=99999) {
         corresp_table <- read_excel("correspondence.xlsx" , sheet = country)
@@ -89,7 +91,6 @@ LAUCHECK_country <- function(country, year=2019, limit=99999) {
         return(output)
 }
 
-
 countrylist <- c("BE","BG","CZ","DK","DE","EE","IE","EL","ES","FR","HR","IT","CY","LV","LT","LU","HU","MT","NL","AT","PL","PT","RO","SI","SK","FI","SE")
 LAUCHECK_results <- lapply(countrylist , LAUCHECK_country)
 
@@ -104,20 +105,10 @@ length <- as.matrix(1:length(countrylist))
 summary_LAUCHECK <- t(as.matrix(apply(length,1,printfct_LAUCHECK)))
 summary_LAUCHECK <- as.data.frame(summary_LAUCHECK)
 colnames(summary_LAUCHECK) <- c("country" , "red_flags")
-View(summary_LAUCHECK)
 
-x<-as.data.frame(summary_NUTSCHECK)
-str(summary_LAUCHECK)
-
-###export summary results
-wb <- createWorkbook("GeoVariables_Checks")
-addWorksheet(wb , "NUTSCHECK")
-writeData(wb , "NUTSCHECK" , summary_NUTSCHECK)
-addWorksheet(wb, "LAUCHECK")
-writeData(wb , "LAUCHECK" , summary_LAUCHECK)
-saveWorkbook(wb , "GeoVariables_Checks" , overwrite = TRUE)
+write.csv(summary_LAUCHECK, "GeoVariables_LAUChecks.csv")
 
 
-saveWorkbook(wb , "//net1.cec.eu.int/Homes/02/marcgab/My Documents/GeoVariables_Checks.xlsx" , overwrite = TRUE)
-write.csv(summary_NUTSCHECK, "GeoVariables_Checks.xlsx")
+### example of a line of command to see the detailed table with red flags
 
+View(NUTSCHECK_results[[17]][[4]])
